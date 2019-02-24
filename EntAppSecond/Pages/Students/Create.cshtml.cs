5fa58172f0b6e5eb0a6689bcda150b02579bc26b
Shane Coppinger <S00190494@mail.itsligo.ie>
2019-02-24 21:00:44 +0000

    Add project files.

---
+++ EntAppSecond/Pages/Students/Create.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EntAppSecond.Pages.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EntAppSecond.Pages.Students
+{
+    public class CreateModel : PageModel
+    {
+        [BindProperty]
+        public Student Student { get; set; }
+
+        public void OnGet()
+        {
+
+        }
+
+        public void OnPost()
+        {
+            if (Student.Email != Student.EmailConfirmation)
+                ModelState.AddModelError("Student.Email", "emails don't match");
+        }
+    }
+}
